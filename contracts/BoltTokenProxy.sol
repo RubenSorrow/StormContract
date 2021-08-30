a9b758ca371e20826e709ff78116032d70a94701
nick <nicolo.vanzo@edu.unito.it>
2021-08-30 11:21:21 +0200

    Added burn and mint functions in the proxy smart contract

--- contracts/BoltTokenProxy.sol
+++ contracts/BoltTokenProxy.sol
@@ -17,8 +17,14 @@ contract BoltTokenProxy is Context {
 
     mapping(address => uint256) private balances;
     mapping(address => mapping(address => uint256)) private allowances;
-    
-    constructor(uint256 _initialSupply, uint256 _currenSupply, string memory _name, string memory _symbol, uint8 _numberOfDecimals) {
+
+    constructor(
+        uint256 _initialSupply,
+        uint256 _currenSupply,
+        string memory _name,
+        string memory _symbol,
+        uint8 _numberOfDecimals
+    ) {
         initialSupply = _initialSupply;
         currentSupply = _currenSupply;
         owner = msg.sender;
@@ -27,65 +33,116 @@ contract BoltTokenProxy is Context {
         numberOfDecimals = _numberOfDecimals;
     }
 
+    modifier onlyAdmin() {
+        require(_msgSender() == owner);
+        _;
+    }
+
     modifier onlyImplementation() {
         require(_msgSender() == implementationAddress);
         _;
     }
 
-    function subtractFunds(address _from, uint256 _value) public onlyImplementation() {
+    function subtractFunds(address _from, uint256 _value)
+        public
+        onlyImplementation
+    {
         balances[_from] = balances[_from].sub(_value);
     }
 
-    function addFunds(address _to, uint256 _value) public onlyImplementation() {
+    function addFunds(address _to, uint256 _value) public onlyImplementation {
         balances[_to] = balances[_to].add(_value);
     }
 
-    function getAddressOfImplementation() external view returns(address){
+    function getAddressOfImplementation() external view returns (address) {
         return implementationAddress;
     }
 
-    function setAddressOfImplementation(address _implementationAddress) public onlyImplementation() {
-        require(_msgSender() == owner, "Only the owner of the contract can set the address of the implementation");
+    function setAddressOfImplementation(address _implementationAddress)
+        public
+        onlyImplementation
+    {
+        require(
+            _msgSender() == owner,
+            "Only the owner of the contract can set the address of the implementation"
+        );
         implementationAddress = _implementationAddress;
     }
 
-    function totalSupply() external view returns(uint256) {
+    function totalSupply() external view returns (uint256) {
         return currentSupply;
     }
 
-    function getInitialSupply() external view returns(uint256) {
+    function getInitialSupply() external view returns (uint256) {
         return initialSupply;
     }
 
-    function getOwner() external view returns(address) {
+    function getOwner() external view returns (address) {
         return owner;
     }
 
-    function name() external view returns(string memory) {
+    function name() external view returns (string memory) {
         return nameOfToken;
     }
 
-    function symbol() external view returns(string memory) {
+    function symbol() external view returns (string memory) {
         return symbolOfToken;
-    }    
+    }
 
-    function decimals() external view returns(uint8) {
+    function decimals() external view returns (uint8) {
         return numberOfDecimals;
     }
 
-    function balanceOf(address _user) external view returns(uint256) {
+    function balanceOf(address _user) external view returns (uint256) {
         return balances[_user];
     }
 
-    function getNumberOfDecimals() external view returns(uint8) {
+    function getNumberOfDecimals() external view returns (uint8) {
         return numberOfDecimals;
     }
 
-    function allowance(address _owner, address _spender) public view returns(uint256) {
+    function allowance(address _owner, address _spender)
+        public
+        view
+        returns (uint256)
+    {
         return allowances[_owner][_spender];
     }
 
-    function _approve(address _owner, address _spender, uint256 _amount) public onlyImplementation() {
+    function _approve(
+        address _owner,
+        address _spender,
+        uint256 _amount
+    ) public onlyImplementation {
         allowances[_owner][_spender] = _amount;
     }
+
+    function mint(address _account, uint256 _amount) public onlyAdmin {
+        _mint(_account, _amount);
+    }
+
+    function _mint(address _account, uint256 _amount) internal {
+        require(_account != address(0), "ERC20: mint to the zero address");
+        currentSupply = currentSupply.add(_amount);
+        balances[_account] = balances[_account].add(_amount);
+        emit Mint(_account, _amount);
+    }
+
+    function burn(address _account, uint256 _amount) public onlyAdmin {
+        _burn(_account, _amount);
+    }
+
+    function _burn(address _account, uint256 _amount) internal {
+        require(_account != address(0), "ERC20: burn to the zero address");
+        uint256 accountBalance = balances[_account];
+        require(accountBalance >= _amount, "ERC20: burn amunt exceeds balance");
+        unchecked {
+            balances[_account] = accountBalance - _amount;
+        }
+        currentSupply = currentSupply.add(_amount);
+        emit Burn(_account, _amount);
+    }
+
+    event Mint(address indexed _account, uint256 _amount);
+    event Burn(address indexed _account, uint256 _amount);
 }
