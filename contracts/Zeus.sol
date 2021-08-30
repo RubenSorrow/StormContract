8857f2a1c8016f966f0f91aae372c8c19d90fbc7
Stefano <stefano.cipolletta@outlook.it>
2021-08-30 11:31:59 +0200

    wrapping functions
    Added wrapper functions for: transfer and transferFrom. 
    Those functions were added because we've seen that for security purposes when a functions is called it triggers another call to another function that does the work. Doing this we're following the pattern designed by the guys of OpenZeppelin.
    Also added increaseAllowance and decreaseAllowance

--- contracts/Zeus.sol
+++ contracts/Zeus.sol
@@ -20,26 +20,58 @@ import "./BoltTokenProxy.sol";
         }
         
         function transfer(address _to, uint256 _value) public returns (bool success) {
+            _transfer(_to, _value);
+            return true;
+        }
+
+        function _transfer(address _to, uint256 _value) internal {
             address msgSender = _msgSender();
             emit Transfer(msgSender, _to, _value);
             myProxy.subtractFunds(msgSender, _value); 
             myProxy.addFunds(_to, _value);
-
-            return true;
         }
 
         function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
-            emit Transfer(_from, _to, _value);
-            myProxy.subtractFunds(_from, _value);
-            myProxy.addFunds(_to, _value);
-
+            _transferFrom(_from, _to, _value);
             return true;
         }
 
+        function _transferFrom(address _from, address _to, uint256 _value) internal{
+            emit Transfer(_from, _to, _value);
+            myProxy.subtractFunds(_from, _value);
+            myProxy.addFunds(_to, _value);
+        }
+
         function approve(address _owner, address _spender, uint256 _amount) public {
+            _approve(_owner, _spender, _amount);
+        }
+
+        function _approve(address _owner, address _spender, uint256 _amount) internal{
             emit Approval(_owner, _spender, _amount);
             myProxy._approve(_owner, _spender, _amount);
         }
+
+        function increaseAllowance(address _owner, address _spender, uint256 _amount) public returns (bool) {
+            _increaseAllowance(_owner, _spender, _amount);
+            return true;
+        }
+
+        function _increaseAllowance(address _owner, address _spender, uint256 _amount) internal {
+            uint256 currentAllowance = myProxy.allowance(_owner, _spender);
+            myProxy._approve(_owner, _spender, currentAllowance+_amount);
+        }
+
+        function decreaseAllowance(address _owner, address _spender, uint256 _amount) public returns (bool){
+            _decreaseAllowance(_owner, _spender, _amount);
+            return true;
+        }
+
+        function _decreaseAllowance(address _owner, address _spender, uint256 _amount) internal {
+            uint256 currentAllowance = myProxy.allowance(_owner, _spender);
+            require(currentAllowance >= _amount, "ERC20: decreased allowance below zero");
+            myProxy._approve(_owner, _spender, currentAllowance - _amount);
+        }
+
 
         event Transfer(address indexed from, address indexed to, uint256 value);
         event Approval(address indexed owner, address indexed spender, uint256 value);
